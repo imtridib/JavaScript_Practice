c942979ed2fc19a20b6825db53e8c7fed9d92e04
Tridib <tridib.ewu.bd@gmail.com>
2024-09-08 21:06:30 +0600

    Some more practices about JavaScript

---
+++ various_params.js
@@ -0,0 +1,36 @@
+/**
+ * for a given string tell me whether it has even number of characters or not
+ *
+ * @format
+ */
+
+function evenSizedString(str) {
+  const size = str.length;
+  console.log(str, size);
+  if (size % 2 === 0) {
+    console.log("even size");
+  } else {
+    console.log("odd size");
+  }
+}
+// evenSizedString("Dhaka");
+// evenSizedString("Taka");
+
+function doubleOrTriple(number, doDouble) {
+  if (doDouble === true) {
+    const result = number * 2;
+    return result;
+  } else {
+    const result = number * 3;
+    return result;
+  }
+}
+
+// console.log(doubleOrTriple(5, true));
+// console.log(doubleOrTriple(5, false));
+
+function numberOfElements(numbers) {
+  const len = numbers.length;
+  return len;
+}
+console.log(numberOfElements([1, 2, 3]));
