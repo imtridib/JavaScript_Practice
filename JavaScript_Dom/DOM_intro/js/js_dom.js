d18f65329bb5afdaebc544160465d915d5f3e2cd
Tridib <tridib.ewu.bd@gmail.com>
2024-09-19 10:13:29 +0600

    Js DOM explore

---
+++ JavaScript_Dom/DOM_intro/js/js_dom.js
@@ -0,0 +1,5 @@
+/** @format */
+
+console.log("hello from JS");
+console.log(document);
+console.log(document.body);
--- JsProblems/JsProblem_Sol.js
+++ JsProblems/JsProblem_Sol.js
@@ -12,12 +12,12 @@ function calculateTax(income, expenses) {
   }
 }
 
-console.log(calculateTax(10000, 3000));
-console.log(calculateTax(34000, 1753));
-console.log(calculateTax(5000, 1500));
-console.log(calculateTax(7000, 7000));
-console.log(calculateTax(-5000, 2000));
-console.log(calculateTax(6000, -1500));
+// console.log(calculateTax(10000, 3000));
+// console.log(calculateTax(34000, 1753));
+// console.log(calculateTax(5000, 1500));
+// console.log(calculateTax(7000, 7000));
+// console.log(calculateTax(-5000, 2000));
+// console.log(calculateTax(6000, -1500));
 
 // problem 2
 function sendNotification(email) {
@@ -34,11 +34,11 @@ function sendNotification(email) {
   }
 }
 
-console.log(sendNotification("zihad@gmail.com"));
-console.log(sendNotification("farhan34@yahoo.com"));
-console.log(sendNotification("nadim.naem5@outlook.com"));
-console.log(sendNotification("fahim234.hotmail.com"));
-console.log(sendNotification("sadia8icloud.com"));
+// console.log(sendNotification("zihad@gmail.com"));
+// console.log(sendNotification("farhan34@yahoo.com"));
+// console.log(sendNotification("nadim.naem5@outlook.com"));
+// console.log(sendNotification("fahim234.hotmail.com"));
+// console.log(sendNotification("sadia8icloud.com"));
 
 // problem 3
 
@@ -56,13 +56,13 @@ function checkDigitsInName(name) {
   }
 }
 
-console.log(checkDigitsInName("Raj123"));
-console.log(checkDigitsInName("n9ayeem"));
-console.log(checkDigitsInName("e1mu3"));
-console.log(checkDigitsInName("Suman"));
-console.log(checkDigitsInName("Name2024"));
-console.log(checkDigitsInName("!@#"));
-console.log(checkDigitsInName(["Raj"]));
+// console.log(checkDigitsInName("Raj123"));
+// console.log(checkDigitsInName("n9ayeem"));
+// console.log(checkDigitsInName("e1mu3"));
+// console.log(checkDigitsInName("Suman"));
+// console.log(checkDigitsInName("Name2024"));
+// console.log(checkDigitsInName("!@#"));
+// console.log(checkDigitsInName(["Raj"]));
 
 // problem 4
 function calculateFinalScore(obj) {
@@ -100,34 +100,34 @@ function calculateFinalScore(obj) {
   }
 }
 
-console.log(
-  calculateFinalScore({
-    name: "Rajib",
-    testScore: 45,
-    schoolGrade: 25,
-    isFFamily: true,
-  })
-);
+// console.log(
+//   calculateFinalScore({
+//     name: "Rajib",
+//     testScore: 45,
+//     schoolGrade: 25,
+//     isFFamily: true,
+//   })
+// );
 
-console.log(
-  calculateFinalScore({
-    name: "Rajib",
-    testScore: 45,
-    schoolGrade: 25,
-    isFFamily: false,
-  })
-);
+// console.log(
+//   calculateFinalScore({
+//     name: "Rajib",
+//     testScore: 45,
+//     schoolGrade: 25,
+//     isFFamily: false,
+//   })
+// );
 
-console.log(calculateFinalScore("hello"));
+// console.log(calculateFinalScore("hello"));
 
-console.log(
-  calculateFinalScore({
-    name: "Rajib",
-    testScore: 15,
-    schoolGrade: 25,
-    isFFamily: true,
-  })
-);
+// console.log(
+//   calculateFinalScore({
+//     name: "Rajib",
+//     testScore: 15,
+//     schoolGrade: 25,
+//     isFFamily: true,
+//   })
+// );
 
 //problem 5
 
@@ -154,10 +154,10 @@ function waitingTime(waitingTimes, serialNumber) {
   }
 }
 
-console.log(waitingTime([3, 5, 7, 11, 6], 10));
-console.log(waitingTime([13, 2], 6));
-console.log(waitingTime([13, 2, 6, 7, 10], 6));
-console.log(waitingTime([6], 4));
-console.log(waitingTime(7, 10));
-console.log(waitingTime("[6,2]", 9));
-console.log(waitingTime([7, 8, 3, 4, 5], "9"));
+// console.log(waitingTime([3, 5, 7, 11, 6], 10));
+// console.log(waitingTime([13, 2], 6));
+// console.log(waitingTime([13, 2, 6, 7, 10], 6));
+// console.log(waitingTime([6], 4));
+// console.log(waitingTime(7, 10));
+// console.log(waitingTime("[6,2]", 9));
+// console.log(waitingTime([7, 8, 3, 4, 5], "9"));
