624a66fe5cbac5bc70ede4418f832ec10bff5067
Tridib <tridib.ewu.bd@gmail.com>
2024-10-05 15:46:51 +0600

    Js DOM EVENTS Summary

--- JavaScript_Dom/DOM_intro/js/first.js
+++ JavaScript_Dom/DOM_intro/js/first.js
@@ -16,9 +16,4 @@ secondSection.style.backgroundColor = "steelblue";
 // document.querySelectorAll
 // document.querySelector
 
-// adding new list
-const secondList = document.getElementById("second-list");
-const li = document.createElement("li");
-li.innerText = "My dynamic item";
 
-secondList.appendChild(li);
--- JavaScript_Dom/JS_DOM_EVENTS/js/events.js
+++ JavaScript_Dom/JS_DOM_EVENTS/js/events.js
@@ -1,3 +1,47 @@
 /** @format */
 
-console.log("This is separate JS filef");
+// option 1: Directly set on the HTML element
+// <button title="This is a Tooltip" onclick="console.log(7)">Click Me</button>
+
+// option 2: add onclick function on the html element
+// IMPORTANT: we will use this
+// <button onclick="makeRed()">Make Red</button>
+function makeRed() {
+  document.body.style.background = "red";
+}
+
+// option: 3
+const makeBlueButton = document.getElementById("make-blue");
+makeBlueButton.onclick = makeBlue;
+function makeBlue() {
+  document.body.style.background = "blue";
+}
+
+// option 3: another [we will use this sometime]
+const purpleButton = document.getElementById("make-purple");
+purpleButton.onclick = function makePurple() {
+  document.body.style.background = "purple";
+};
+
+// option: 4
+const pinkButton = document.getElementById("make-pink");
+pinkButton.addEventListener("click", makePink);
+
+function makePink() {
+  document.body.style.backgroundColor = "pink";
+}
+
+// option: 4 another
+const makeGreenButton = document.getElementById("make-green");
+makeGreenButton.addEventListener("click", function makeGreen() {
+  document.body.style.background = "green";
+});
+
+// option: 4 Final
+// IMPORTANT: we will use this sometimes
+//   document.getElementById('make-goldenrod').addEventListener('click', function(){})
+document
+  .getElementById("make-goldenrod")
+  .addEventListener("click", function () {
+    document.body.style.backgroundColor = "goldenrod";
+  });
