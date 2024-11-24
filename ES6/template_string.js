312389dc83515855aebd0329de3b63c09201fb46
Tridib <tridib.ewu.bd@gmail.com>
2024-11-25 05:47:12 +0600

    ES6 Study

---
+++ ES6/template_string.js
@@ -0,0 +1,20 @@
+/** @format */
+
+const a = 10;
+const b = 20;
+const result = "The sum of " + a + " and " + b + " is " + (a + b);
+
+console.log(result);
+
+//ES6 Template String Examples
+const math = `The sum of ${a} and ${b} is ${a + b}`;
+console.log(math);
+
+const email = "Hii, How are you doing! \n" + "lets go for a walk.";
+console.log(email);
+
+//ES6 Template String Examples
+const challenge = `Hii, How are you doing!
+Lets go for a walk.
+And have some good coffee`;
+console.log(challenge);
