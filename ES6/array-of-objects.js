49b4aee405beb7f70a461d41db2d2a5c05b9a77f
Tridib <tridib.ewu.bd@gmail.com>
2024-11-27 23:50:51 +0600

    ES6 Study more examples

---
+++ ES6/array-of-objects.js
@@ -0,0 +1,12 @@
+/** @format */
+
+const products = [
+  { id: 1, name: "lenovo", price: 65000 },
+  { id: 2, name: "dell", price: 45000 },
+  { id: 3, name: "hp", price: 40000 },
+  { id: 4, name: "mac", price: 150000 },
+];
+
+//map
+const names = products.map((product) => product.name);
+console.log(names);
