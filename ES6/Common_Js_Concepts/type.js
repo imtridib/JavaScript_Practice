ac5f932fc6ef3e1f74a5c4a4ff5799fe05214156
Tridib <tridib.ewu.bd@gmail.com>
2024-11-28 23:46:51 +0600

    ES6 Common Js concepts

--- ES6/Common_Js_Concepts/type.js
+++ ES6/Common_Js_Concepts/type.js
@@ -12,3 +12,6 @@
 
 const a = 5;
 const b = "Some Common Js Concepts";
+const ages = [45, 65, 48];
+const student = { id: 23, class: 7 };
+console.log(typeof a, typeof b, typeof ages, typeof student);
