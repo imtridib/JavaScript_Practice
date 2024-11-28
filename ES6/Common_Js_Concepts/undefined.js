1d3e9109f2d97518ce5c3792aa42f50596f3e662
Tridib <tridib.ewu.bd@gmail.com>
2024-11-29 01:34:59 +0600

    ES6 Common Js concepts undefined js

--- ES6/Common_Js_Concepts/undefined.js
+++ ES6/Common_Js_Concepts/undefined.js
@@ -1,11 +1,19 @@
 /**
  * 8 ways to get undefine
- *  1. variable that is not initialized will give undefined
+ * 1. variable that is not initialized will give undefined
  * 2. function with no return
+ * 3. parameter that is not passed will be undefined
+ * 4. if return has nothing on the right side will return undefined
+ * 5. Property that doesn't exists on an object will give you undefined
+ * 6. accessing array elements outside of the index range
+ * 7. deleting an element inside an array
+ * 8. set a value directly to undefined
  * @format
  */
 
 let first;
+console.log(first);
+
 function second(a, b) {
   const total = a + b;
 }
@@ -19,6 +27,33 @@ function third(a, b, c, d) {
 
 third(2, 5);
 
-console.log(result);
+// console.log(result);
 
-console.log(first);
+function noNegative(a, b) {
+  if (a < 0 || b < 0) {
+    return;
+  }
+  return a + b;
+}
+
+const total = noNegative(2, -5);
+// console.log(total);
+
+const fifth = { id: 2, name: "ponchom", age: 40 };
+// console.log(fifth.age, fifth.salary);
+
+const sixth = [4, 89, 87, 56, 54];
+console.log(sixth[1], sixth[5], sixth[200]);
+
+// Should not do it. Instead use splice
+
+delete sixth[1];
+
+const eighth = undefined;
+
+const ninth = null;
+
+const data = { results: [], updated: null };
+
+console.log(typeof undefined);
+console.log(typeof null);
