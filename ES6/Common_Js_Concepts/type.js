58759aadee31c7f5a30acf6307dbcfddb474cb04
Tridib <tridib.ewu.bd@gmail.com>
2024-11-28 23:53:58 +0600

    ES6 Common Js concepts

--- ES6/Common_Js_Concepts/type.js
+++ ES6/Common_Js_Concepts/type.js
@@ -10,8 +10,24 @@
 
 // dynamic type Language
 
+// JavaScript is a dynamic typed Language
+
+// Primitive type
 const a = 5;
 const b = "Some Common Js Concepts";
+const d = true;
+
+// non-primitive
 const ages = [45, 65, 48];
 const student = { id: 23, class: 7 };
-console.log(typeof a, typeof b, typeof ages, typeof student);
+
+// console.log(typeof a, typeof b, typeof ages, typeof student);
+
+let x = 5;
+let y = x;
+console.log(x, y);
+y = 7;
+console.log(x, y);
+
+let p = { job: "web developer" };
+let q = p;
