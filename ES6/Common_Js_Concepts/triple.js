d46a8de9f90235ca985892209c2294ba25ddf002
Tridib <tridib.ewu.bd@gmail.com>
2024-11-29 09:43:47 +0600

    ES6 Common Js concepts triple equal concepts

---
+++ ES6/Common_Js_Concepts/triple.js
@@ -0,0 +1,30 @@
+/** @format */
+
+// const first = 2;
+// const second = "2";
+
+// const first = 1;
+// const second = true;
+
+// const first = "0";
+// const second = false;
+
+const first = [];
+const second = [];
+
+const first = { x: 5 };
+const second = { x: 5 };
+if (first == second) {
+  console.log("values are equal");
+} else {
+  console.log("values are note equal");
+}
+if (first === second) {
+  console.log("values are equal");
+} else {
+  console.log("values are note equal");
+}
+
+// type coercion
+// type conversion
+// type casting
