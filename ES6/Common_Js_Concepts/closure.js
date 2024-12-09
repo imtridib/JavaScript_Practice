bede548f9a028ec180fa08905f5a8bd6cf4ed2fb
Tridib <tridib.ewu.bd@gmail.com>
2024-12-10 01:09:20 +0600

    ES6 Common Js concepts closure

--- ES6/Common_Js_Concepts/closure.js
+++ ES6/Common_Js_Concepts/closure.js
@@ -3,6 +3,7 @@
 function stopWatch() {
   let counter = 0;
   return function () {
+    // New function return inside the function work like new for every function
     counter++;
     return counter;
   };
