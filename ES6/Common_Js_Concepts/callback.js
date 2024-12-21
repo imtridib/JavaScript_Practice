31d8528636946b2069aa426dafa64b413c23bf03
Tridib <tridib.ewu.bd@gmail.com>
2024-12-21 13:42:43 +0600

    ES6 Common Js concepts callBack Function

---
+++ ES6/Common_Js_Concepts/callback.js
@@ -0,0 +1,25 @@
+/** @format */
+
+function greeting(greetingHandler, name) {
+  greetingHandler(name);
+}
+
+function greetingHandler(name) {
+  console.log("Good Morning", name);
+}
+
+function greetEvening(name) {
+  console.log("Good Evening", name);
+}
+
+greeting(greetingHandler, "Jack");
+greeting(greetingHandler, "Tom");
+
+greeting(greetEvening, "Jery");
+
+// callBack Function uses
+function submitHandler() {
+  console.log("submit button clicked");
+}
+
+document.getElementById("btn-submit").addEventListener("click", submitHandler);
