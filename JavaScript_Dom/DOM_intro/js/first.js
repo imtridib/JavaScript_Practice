29c7396fc43a160e3322ad47c418752120cb0770
Tridib Chakraborty <tridib.ewu.bd@gmail.com>
2025-04-27 00:27:53 +0600

    JavaScript DOM practice

--- JavaScript_Dom/DOM_intro/js/first.js
+++ JavaScript_Dom/DOM_intro/js/first.js
@@ -14,6 +14,25 @@ secondSection.style.color = "White";
 secondSection.style.backgroundColor = "steelblue";
 
 // document.querySelectorAll
+// example:
+{/* <p>Add a background color all elements with class="example":</p>
+<h2 class="example">A heading</h2>
+<p class="example">A paragraph.</p> 
+
+<script>
+const nodeList = document.querySelectorAll(".example");
+</script> */}
+
 // document.querySelector
+// example:
+{/* <p>Add a background color to the first element with class="example":</p>
+
+<p class="example">I am a paragraph.</p>
+<p class="example">I am a paragraph.</p> 
+
+<script>
+document.querySelector(".example").style.backgroundColor = "red";
+</script> */}
+
 
 
