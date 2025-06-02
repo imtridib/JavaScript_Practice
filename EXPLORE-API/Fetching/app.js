91352679faa3168b8bdf37d2b7ba645f38d79ef4
Tridib Chakraborty <tridib.ewu.bd@gmail.com>
2025-06-02 23:08:03 +0600

    API learning

--- EXPLORE-API/Fetching/app.js
+++ EXPLORE-API/Fetching/app.js
@@ -1,1 +1,6 @@
-console.log("app.js");
+function loadData2() {
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
